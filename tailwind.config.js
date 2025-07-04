1fa092352bead02d86d0cd0189d2ae198ee3cd49
Yefee8 <yavuzefefindikli8@gmail.com>
2025-07-04 16:26:23 +0300

    getting started

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                background: "#FFFCFB",
+                foreground: "#222831",
+                primary: "#093FB4",
+            }
+        },
+    },
+    plugins: [],
+};
