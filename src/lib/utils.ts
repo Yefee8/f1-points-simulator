011f67c1109e40f54a413cec04f393fc1e68b22e
yefee8 <yavuzefefindikli8@gmail.com>
2025-07-07 17:55:50 +0300

    -

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { getDriverStandings, getTeamLineup } from "@/lib/f1-api/src/server";
+// import { getDriverStandings, getTeamLineup } from "f1-api-node";
 import { Standing } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
@@ -8,15 +8,17 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export async function getActiveStandings() {
-  const activeStandings: Standing[] = await getDriverStandings();
-  const activeTeamLineup = await getTeamLineup();
+  // const activeStandings: Standing[] = await getDriverStandings();
+  // const activeTeamLineup = await getTeamLineup();
 
-  activeStandings.forEach((standing) => {
-    const team = activeTeamLineup.find((team) => team.name === standing.team);
-    if (team) {
-      standing.carLogo = team.carLogo;
-    }
-  });
+  // activeStandings.forEach((standing) => {
+  //   const team = activeTeamLineup.find((team) => team.name === standing.team);
+  //   if (team) {
+  //     standing.carLogo = team.carLogo;
+  //   }
+  // });
+
+  const activeStandings: Standing[] = [];
 
   return activeStandings;
 }
